d3ad8be3125ed90918213dd093685d08f77ea162
Your Name <your-email@example.com>
2026-03-08 18:51:06 +0530

    chore: migrate app paths and update subproject pointer

--- app/dashboard/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import DashboardProvider from "./Provider";
-
-function DashboardLayout({children}: any) {
-    return (
-        <DashboardProvider>{children}</DashboardProvider>
-    )
-}
-export default DashboardLayout;
